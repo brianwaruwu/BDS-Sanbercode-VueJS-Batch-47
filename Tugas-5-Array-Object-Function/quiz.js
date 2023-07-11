7f2171d14027f25393f0972b57bde8979580f3de
brianwaruwu <brianwar03@gmail03.com>
2023-07-11 08:59:45 +0700

    Tugas_6_ES6

--- Tugas-5-Array-Object-Function/quiz.js
+++
@@ -1,9 +0,0 @@
-var i = 5;
-
-if (i = 5) {
-    // Kode yang akan dieksekusi jika i bernilai 5
-    console.log("Nilai i sama dengan 5");
-} else {
-    // Kode yang akan dieksekusi jika i tidak bernilai 5
-    console.log("Nilai i bukan 5");
-}
